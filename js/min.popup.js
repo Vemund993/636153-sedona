bb2089c4dd93b485e962b410b690a1c5178eff19
Vemund993 <36744431+Vemund993@users.noreply.github.com>
2018-04-01 21:55:18 +0300

    коммит

---
+++ js/min.popup.js
@@ -0,0 +1,1 @@
+var link=document.querySelector(".button-interesting"),popup=document.querySelector(".interesting-form");link.addEventListener("click",function(e){e.preventDefault(),popup.classList.toggle("form-show")});
