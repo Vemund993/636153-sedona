3debf0a2dd224b5941ce068ea55ea04e50e61fbe
Vemund993 <36744431+Vemund993@users.noreply.github.com>
2018-04-01 16:37:49 +0300

    JS

---
+++ js/popup.js
@@ -0,0 +1,6 @@
+var link = document.querySelector(".button-interesting");
+var popup = document.querySelector(".interesting-form");
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("form-show");
+});
