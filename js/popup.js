b519232203f25d2391ee0df386691069082b13ad
Vemund993 <36744431+Vemund993@users.noreply.github.com>
2018-04-08 21:37:28 +0300

    commit

--- js/popup.js
+++ js/popup.js
@@ -4,3 +4,28 @@ link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("form-show");
 });
+
+@keyframes modal-show {
+0% {
+ -webkit-transform:translateY(-100px);
+ transform:translateY(-100px)
+}
+100% {
+ -webkit-transform:translateY(0);
+ transform:translateY(0)
+}
+}- это для выезда формы вниз
+@keyframes modal-error {
+0%,100% {
+ -webkit-transform:translateX(0);
+ transform:translateX(0)
+}
+10%,30%,50%,70% {
+ -webkit-transform:translateX(10px);
+ transform:translateX(10px)
+}
+20%,40%,60%,80% {
+ -webkit-transform:translateX(-10px);
+ transform:translateX(-10px)
+}
+}
